afb2d95c473ef513134e72822b123c63e6251f35
aulisius <aulisius7@gmail.com>
2023-06-21 11:56:37 +0530

    Add missing capabilites to type definition

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,16 @@ export interface DefaultSdk {
   wpversion: string;
 }
 
+type Capabilities =
+  | "hasEcomdash"
+  | "hasYithExtended"
+  | "isEcommerce"
+  | "isJarvis"
+  | "canAccessHelpCenter"
+  | "canAccessAI";
+
 interface RuntimeSdk {
-  hasCapability: (
-    name: "hasEcomdash" | "hasYithExtended" | "isEcommerce" | "isJarvis"
-  ) => boolean;
+  hasCapability: (name: Capabilities) => boolean;
   adminUrl: (path: string) => string;
   createApiUrl: (path: string, qs?: Record<string, any>) => string;
   siteDetails: { url: string; title: string };
