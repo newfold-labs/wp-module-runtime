c6ec9a0dfc4d749bde176779b64d1d235bae5847
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-10-03 21:46:43 +0530

    requestedd changes

--- bootstrap.php
+++ bootstrap.php
@@ -5,16 +5,13 @@ use NewfoldLabs\WP\Module\Runtime\Runtime;
 
 use function NewfoldLabs\WP\ModuleLoader\register;
 
-			register(
-				array(
-					'name'     => 'runtime',
-					'label'    => __( 'Runtime', 'wp-module-runtime' ),
-					'callback' => function ( Container $container ) {
-						$runtime = new Runtime( $container );
-						$runtime->loadIntoPage( 'admin_enqueue_scripts' );
-					},
-					'isActive' => true,
-					'isHidden' => true,
-				)
-			);
+if ( function_exists( 'add_action' ) )  {
+	add_action(
+		'newfold_container_set',
+		function ( $container )  {
+                        $runtime = new Runtime( $container );
+                        $runtime->loadIntoPage( 'admin_enqueue_scripts' );
+               }
+        );
+}
 
